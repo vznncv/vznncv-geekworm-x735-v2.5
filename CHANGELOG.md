0bab0ac112e934013da861aef3e763f356203f19
Konstantin Kochin <vznncv@gmail.com>
2021-12-12 22:46:25 +0300

    Refactor `geekworm-x735-power.py` script and service
    1. Refactor `geekworm-x735-power.py` to use *sysfs* instead of *pigpio*. It helps to avoid problems with network availability during shutdown.
       Note: GPIO pins 12 and 20 are switched from input pull up/down to output mode, as sysfs doesn't support pull configuration.
    2. Update systemd poweroff/reboot service hooks to poweroff/reboot system correctly

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,13 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+## [Unreleased]
+### Changed
+- Refactor `geekworm-x735-power.py` script to use *sysfs* instead of *pigpio*. It helps to avoid problems with
+  network availability during shutdown.
+### Fixed
+- Fixed systemd safe shutdown/reboot hooks.
+
 ## [0.1.0] - 2021-09-12
 ### Added
 - Added shield fan control service
--- geekworm-x735-power.py
+++ geekworm-x735-power.py
@@ -6,117 +6,244 @@ import argparse
 import logging
 import os
 import subprocess
-import sys
 import time
-
-import pigpio
 
 logger = logging.getLogger("power-service")
 
-# boot indication pin
-_x735_BOOT_INFO_GPIO = 12
-# button pin
-_x735_SHUTDOWN_BUTTON_GPIO = 20
-# shutdown info pin
-_x735_SHUTDOWN_SIGNAL_GPIO = 5
+
+#
+# Minimal native GPIO control implementation.
+#
+
+class _SYSFSGPIOManager:
+    _SYSFS_GPIO_PATH = '/sys/class/gpio'
+
+    def __init__(self):
+        pass
+
+    @classmethod
+    def _check_pin(cls, pin):
+        if not 0 <= pin < 54:
+            raise ValueError(f"Unknown pin number {pin}")
+
+    def _export_and_get_pin_path(self, pin):
+        self._check_pin(pin)
+        gpio_path = os.path.join(self._SYSFS_GPIO_PATH, f'gpio{pin}')
+        if os.path.exists(gpio_path):
+            return gpio_path
+        # try to export pin
+        with open(os.path.join(self._SYSFS_GPIO_PATH, 'export'), 'w') as f:
+            f.write(str(pin))
+        if not os.path.exists(gpio_path):
+            raise ValueError(f"Fail to activate pin {pin}")
+        return gpio_path
+
+    def _gpio_direction_path(self, pin):
+        return os.path.join(self._export_and_get_pin_path(pin), 'direction')
+
+    def _gpio_value_path(self, pin):
+        return os.path.join(self._export_and_get_pin_path(pin), 'value')
+
+    def get_mode(self, pin):
+        gpio_direction_path = self._gpio_direction_path(pin)
+        with open(gpio_direction_path) as f:
+            value = f.read().strip()
+        if value == "in":
+            return "input"
+        elif value == "out":
+            return "output"
+        else:
+            raise ValueError(f"Unknown pin mode: {value}")
+
+    def set_mode(self, pin, mode):
+        gpio_direction_path = self._gpio_direction_path(pin)
+        if mode == "input":
+            value = "in"
+        elif mode == "output":
+            value = "out"
+        else:
+            raise ValueError(f"Unknown mode: {mode}")
+        prev_value = self.get_mode(pin)
+        if prev_value != value:
+            with open(gpio_direction_path, 'w') as f:
+                return f.write(value)
+
+    def set_value(self, pin, value):
+        gpio_value_path = self._gpio_value_path(pin)
+        with open(gpio_value_path, 'w') as f:
+            f.write(str(value))
+
+    def get_value(self, pin):
+        gpio_value_path = self._gpio_value_path(pin)
+        with open(gpio_value_path, 'r') as f:
+            raw_value = f.read()
+        return int(raw_value.strip())
 
 
-def _activate_boot_pin(pi):
-    # pull up boot pin for power up indication
-    logger.info(f"Activate boot into GPIO ({_x735_BOOT_INFO_GPIO})")
-    pi.set_mode(_x735_BOOT_INFO_GPIO, pigpio.INPUT)
-    pi.set_pull_up_down(_x735_BOOT_INFO_GPIO, pigpio.PUD_UP)
+#
+# X735 power manager
+#
 
-    if pi.read(_x735_BOOT_INFO_GPIO) == 0:
-        logger.warning(f"Fail to set boot info GPIO {_x735_BOOT_INFO_GPIO} to 1")
+class _PowerManagerX735:
+    # boot indication pin
+    BOOT_INFO_GPIO = 12
+    # shutdown control pin
+    SHUTDOWN_CONTROL_GPIO = 20
+    # shutdown signal pin
+    SHUTDOWN_SIGNAL_GPIO = 5
 
+    def __init__(self):
+        self.gpio = _SYSFSGPIOManager()
 
-_x735_SHUTDOWN_PULSE = 4
-_x735_SHUTDOWN_DELAY = 1.5
+    def _activate_pin(self, pin, name, mode, value=0):
+        prev_mode = self.gpio.get_mode(pin)
+        prev_value = self.gpio.get_value(pin)
+        if mode != prev_mode or (mode != 'input' and value != prev_value):
+            logger.info(f"Activate/reset {name} pin ({pin}, mode: {mode})")
+            self.gpio.set_mode(pin, mode)
+            if mode != 'input':
+                self.gpio.set_value(pin, value)
 
+    def init_pins(self):
+        """
+        Reset power control pins to default values.
+        """
+        # reset shutdown control pin
+        self._activate_pin(self.SHUTDOWN_CONTROL_GPIO, 'showdown control', 'output', 0)
+        # reset shutdown info pin
+        self._activate_pin(self.SHUTDOWN_SIGNAL_GPIO, 'shutdown signal', 'input')
+        # activate indication pin
+        self._activate_pin(self.BOOT_INFO_GPIO, 'boot info', 'output', 1)
 
-def _safe_shutdown():
-    logger.info("Send shutdown signal to x735 ...")
+    def _send_shutdown_signal(self, signal_name, pulse_length, post_pulse_delay=1):
+        # ensure that pins are initialized
+        self.init_pins()
 
-    pi = pigpio.pi()
+        # send signal
+        logger.info(f"Send {signal_name} signal to x735 ...")
+        self.gpio.set_value(self.SHUTDOWN_CONTROL_GPIO, 1)
+        time.sleep(pulse_length)
+        self.gpio.set_value(self.SHUTDOWN_CONTROL_GPIO, 0)
+        time.sleep(post_pulse_delay)
 
-    # ensure that boot pin is active
-    _activate_boot_pin(pi)
+        logger.info("Complete")
 
-    # user input with pull up to emulate open drain
-    pi.set_mode(_x735_SHUTDOWN_BUTTON_GPIO, pigpio.INPUT)
-    pi.set_pull_up_down(_x735_SHUTDOWN_BUTTON_GPIO, pigpio.PUD_UP)
-    time.sleep(_x735_SHUTDOWN_PULSE)
-    pi.set_pull_up_down(_x735_SHUTDOWN_BUTTON_GPIO, pigpio.PUD_DOWN)
-    # small delay to activate hardware
-    time.sleep(_x735_SHUTDOWN_DELAY)
+    def safe_poweroff(self):
+        """
+        Safe shutdown.
+        """
+        self._send_shutdown_signal(signal_name="shutdown", pulse_length=4)
 
-    logger.info("Complete")
+    def safe_reboot(self):
+        """
+        Safe reboot.
+        """
+        self._send_shutdown_signal(signal_name="reboot", pulse_length=1)
 
+    SHUTDOWN_SIGNAL_LOW_PULL_PERIOD = 0.2
+    SHUTDOWN_SIGNAL_HIGH_PULL_PERIOD = 0.02
 
-_x735_REBOOT_PULSE = 0.2
-_x735_POWEROFF_PULSE = 0.6
-_X735_SHUTDOWN_SIGNAL_LOW_PULL_PERIOD = 0.2
-_X735_SHUTDOWN_SIGNAL_HIGH_PULL_PERIOD = 0.02
+    SHUTDOWN_SIGNAL_REBOOT_PULSE = 0.2
+    SHUTDOWN_SIGNAL_POWEROFF_PULSE = 0.6
 
+    SHUTDOWN_TIMEOUT = 30
 
-def _monitor_momentary_button():
-    pi = pigpio.pi()
+    def monitor(self):
+        """
+        Monitor shutdown/reboot signal.
+        """
+        try:
 
-    # ensure that boot pin is active
-    _activate_boot_pin(pi)
+            while True:
+                self._monitor_loop()
+                time.sleep(self.SHUTDOWN_TIMEOUT)
+                logger.warning(
+                    f"System haven't been shut down after {self.SHUTDOWN_TIMEOUT} seconds. Continue monitoring!"
+                )
+        except KeyboardInterrupt:
+            logger.info("Shutdown ...")
 
-    pi.set_mode(_x735_SHUTDOWN_SIGNAL_GPIO, pigpio.INPUT)
+    def _shutdown_system(self, shutdown_command):
+        result = subprocess.run(['systemctl', 'is-system-running'], encoding='utf-8', stdout=subprocess.PIPE)
+        state = result.stdout.strip()
+        # don't call any shutdown command if system is stopping
+        if state == 'stopping':
+            return
+        if shutdown_command == 'poweroff':
+            subprocess.check_output(['shutdown', '--poweroff', 'now'])
+        elif shutdown_command == 'reboot':
+            subprocess.check_output(['shutdown', '--reboot', 'now'])
+        else:
+            raise ValueError(f"Unknown shutdown command: {shutdown_command}")
 
-    logger.info(f"Start power control GPIO ({_x735_SHUTDOWN_SIGNAL_GPIO}) monitoring")
-    try:
+    def _monitor_loop(self):
+        """
+        Monitor shutdown/reboot signal.
+        """
+        # ensure that pins are initialized
+        self.init_pins()
+
+        logger.info(f"Start power control pin ({self.SHUTDOWN_SIGNAL_GPIO}) monitoring")
+
         while True:
-            time.sleep(_X735_SHUTDOWN_SIGNAL_LOW_PULL_PERIOD)
-            shutdown_signal = pi.read(_x735_SHUTDOWN_SIGNAL_GPIO)
+            time.sleep(self.SHUTDOWN_SIGNAL_LOW_PULL_PERIOD)
+            shutdown_signal = self.gpio.get_value(self.SHUTDOWN_SIGNAL_GPIO)
             if not shutdown_signal:
                 continue
 
+            logger.info("Detect pulse start")
             pulse_start = time.time()
-            logger.info(f"Detect pulse start")
-            while shutdown_signal:
-                time.sleep(_X735_SHUTDOWN_SIGNAL_HIGH_PULL_PERIOD)
+            time.sleep(self.SHUTDOWN_SIGNAL_HIGH_PULL_PERIOD)
 
+            while True:
                 pulse_time = time.time() - pulse_start
-                if pulse_time > _x735_POWEROFF_PULSE:
+                pulse_time_ms = int(pulse_time * 1000)
+                shutdown_signal = self.gpio.get_value(self.SHUTDOWN_SIGNAL_GPIO)
+                if not shutdown_signal:
+                    break
+
+                if pulse_time > self.SHUTDOWN_SIGNAL_POWEROFF_PULSE:
                     # reboot pulse
-                    logger.info(f"X735 Shutting down (pulse time >= {int(pulse_time * 1000)} ms) ...")
-                    subprocess.check_call(['shutdown', '--poweroff', 'now'])
-                    sys.exit(0)
+                    logger.info(f"Shutting down (pulse time >= {pulse_time_ms} ms) ...")
+                    self._shutdown_system('poweroff')
+                    return
+                time.sleep(self.SHUTDOWN_SIGNAL_HIGH_PULL_PERIOD)
 
-                shutdown_signal = pi.read(_x735_SHUTDOWN_SIGNAL_GPIO)
-
-            pulse_time = time.time() - pulse_start
-            if pulse_time > _x735_REBOOT_PULSE:
+            if pulse_time > self.SHUTDOWN_SIGNAL_REBOOT_PULSE:
                 logger.info(f"X735 Rebooting (pulse time {int(pulse_time * 1000)} ms)...")
-                subprocess.check_call(['shutdown', '--reboot', 'now'])
-                sys.exit(0)
+                self._shutdown_system('reboot')
+                return
             else:
                 logger.info(f"Invalid control pulse ({int(pulse_time * 1000)} ms)")
 
-    except KeyboardInterrupt:
-        logger.info("Shutdown ...")
 
+#
+# CLI interface
+#
 
 def main(args=None):
     parser = argparse.ArgumentParser(description='geekwork x735 hat fan daemon')
-    parser.add_argument('command', help="power command", choices=['monitor', 'safe-shutdown'])
+    parser.add_argument('command', help="power command", choices=[
+        'monitor',
+        'safe-shutdown', 'safe-poweroff',
+        'safe-reboot'
+    ])
     parsed_args = parser.parse_args(args)
     logging.basicConfig(level=logging.INFO, format='%(asctime)s  [%(name)s:%(lineno)s]  %(levelname)s - %(message)s')
 
     if os.geteuid() != 0:
         raise ValueError("script should be run with root permissions")
 
+    power_manager = _PowerManagerX735()
+
     if parsed_args.command == 'monitor':
-        _monitor_momentary_button()
-    elif parsed_args.command == 'safe-shutdown':
-        _safe_shutdown()
+        power_manager.monitor()
+    elif parsed_args.command == 'safe-shutdown' or parsed_args.command == 'safe-poweroff':
+        power_manager.safe_poweroff()
+    elif parsed_args.command == 'safe-reboot':
+        power_manager.safe_reboot()
     else:
-        raise ValueError("Unknown command")
+        raise ValueError(f"Unknown command: {parsed_args.command}")
 
 
 if __name__ == '__main__':
